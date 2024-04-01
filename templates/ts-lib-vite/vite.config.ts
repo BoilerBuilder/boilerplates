2d9a2239ea9de12515bcfea0306859b9a8a4bc79
Gab <gabriel@laplanta.com.br>
2024-04-01 04:22:15 -0300

    fix: eslint and prettier on ts-lib-vite

--- templates/ts-lib-vite/vite.config.ts
+++ templates/ts-lib-vite/vite.config.ts
@@ -1,24 +1,22 @@
-import { defineConfig } from 'vite'
-import path from 'path'
-import dts from 'vite-plugin-dts'
+import { defineConfig } from 'vite';
+import path from 'path';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    dts({ include: ['src'] })
-  ],
+  plugins: [dts({ include: ['src'] })],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/main.ts'),
-      formats: ['es']
+      formats: ['es'],
     },
     rollupOptions: {
       external: [],
-    }
+    },
   },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
-  }
-})
+  },
+});
