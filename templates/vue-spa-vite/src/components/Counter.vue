585133d955258c8dde276a332bcb37765366fea8
Gab <gabriel@laplanta.com.br>
2024-04-19 23:26:32 -0300

    feat: Including unfinished vue-spa-vite template

---
+++ templates/vue-spa-vite/src/components/Counter.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <p>Count: {{ count }}</p>
+        <button @click="increment">Increment</button>
+    </div>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+    setup() {
+        const count = ref(0);
+
+        function increment() {
+            count.value++;
+        }
+
+        return {
+            count,
+            increment,
+        };
+    },
+});
+</script>
