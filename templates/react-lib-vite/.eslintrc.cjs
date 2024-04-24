c9958223aebe119f3abf22d956b0fe5d301a4d8f
Gab <gabriel@laplanta.com.br>
2024-04-23 22:59:42 -0300

    feat: Removing react-refresh eslint rule

--- templates/react-lib-vite/.eslintrc.cjs
+++ templates/react-lib-vite/.eslintrc.cjs
@@ -1,15 +1,21 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
     'plugin:prettier/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: [
+    'node_modules/', // Ignore all files in the node_modules directory
+    'build/', // Ignore all files in the build directory
+    'dist/', // Ignore all files in the dist directory
+    'coverage/', // Ignore all files in the coverage directory
+    '.yalc/', // Ignore all files that end with .test.js
+  ],
   parser: '@typescript-eslint/parser',
-  plugins: ['react', 'import', 'react-refresh'],
+  plugins: ['react', 'import'],
   rules: {
     'linebreak-style': 0, // Desliga o uso lineabreak para evitar conflito de diferentes OS
     quotes: [
@@ -32,9 +38,5 @@ module.exports = {
       },
     ],
     'react/jsx-sort-props': 'error', // Força a ordenação das propriedades do JSX
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
   },
 };
